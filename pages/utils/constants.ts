05ec2cae7f18b401a83e0aff0badd1c5e688cd28
TomGegs <115059119+TomGegs@users.noreply.github.com>
2023-07-12 17:12:13 +1000

    Utils location incorrect - removing duplicate

--- pages/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Configuration } from "openai";
-
-const configuration = new Configuration({
-    organization: "org-A7fKbkj4Fgp4Hf8aSRCuzNe0",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-export default configuration;
